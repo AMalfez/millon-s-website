fc02fdedd200a1a3a76354362bb2444f33bb4b28
AMalfez <a_mansuri@ch.iitr.ac.in>
2025-01-31 15:35:44 +0530

    fixed prod bug

--- components/Blog/BlogImage.tsx
+++ components/Blog/BlogImage.tsx
@@ -1,8 +1,8 @@
 "use client";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import React from "react";
 
-function BlogImage({ src, alt }: {src:string, alt:string}) {
+function BlogImage({ src, alt }: { src: StaticImageData, alt: string }) {
   return (
     <>
       <Image src={src} alt={alt} className="object-cover w-full my-5" />
