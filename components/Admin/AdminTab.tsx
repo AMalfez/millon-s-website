b34aa14238c7aa842a5efd1d131d8a3a34b41e37
AMalfez <a_mansuri@ch.iitr.ac.in>
2025-02-11 11:54:12 +0530

    fixed a prod bug

--- components/Admin/AdminTab.tsx
+++ components/Admin/AdminTab.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { CardElement, CardElementHorizontal } from "../Events/Card";
-import { Event, initialState } from "@/reducers/EventReducer";
+// import { CardElement, CardElementHorizontal } from "../Events/Card";
+// import { Event, initialState } from "@/reducers/EventReducer";
 
 function AdminTab() {
   return (
@@ -12,10 +12,10 @@ function AdminTab() {
         <TabsTrigger value="past_events">Past Events</TabsTrigger>
       </TabsList>
       <TabsContent value="current_events" className="flex-col hidden md:flex gap-2">
-        {initialState.map((e:Event, index:number)=>(<CardElementHorizontal key={index} event={e} />))}
+        {/* {initialState.map((e:Event, index:number)=>(<CardElementHorizontal key={index} event={e} />))} */}
       </TabsContent>
       <TabsContent value="current_events" className="grid grid-cols-1 md:hidden gap-2">
-        {initialState.map((e:Event, index:number)=>(<CardElement key={index} event={e} />))}
+        {/* {initialState.map((e:Event, index:number)=>(<CardElement key={index} event={e} />))} */}
       </TabsContent>
       <TabsContent value="upcoming_events" className="flex-col flex gap-2">
         <p className="text-center mt-20 text-[#b0b0b0]">There are currently no Upcoming Events</p>
