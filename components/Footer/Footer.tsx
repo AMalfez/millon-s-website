8f43521ecf34e7b27e1c3aa255697a72c9e0beda
AMalfez <a_mansuri@ch.iitr.ac.in>
2025-02-14 11:05:25 +0530

    integrated mailchimp

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,12 +3,49 @@ import React from "react";
 import Link from "next/link";
 import Logo from "@/public/assets/06/logo_horizontal-300x125.png";
 import Image from "next/image";
+import { useToast } from "@/hooks/use-toast"
 
 interface FooterProps {
   background?: string;  
 }
 
 const Footer: React.FC<FooterProps> = () => {
+  const { toast } = useToast()
+  const [email, setEmail] = React.useState("");
+  const handleSignup = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {    
+    const response = await fetch("/api/subscribe", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+      }),
+    });
+    const datas = await response.json();
+    console.log(datas);
+    if(datas.status==='subscribed'){
+      toast({
+        title:"Subscription Confirmed",
+        description:"You have successfully subscribed to our newsletter."
+      })
+      setEmail("");
+    }else{
+      toast({
+        title:"Subscription Failed",
+        description:"OOPS! some error occured. Please try again later."
+      })
+    }
+  } catch (error) {
+      console.log(error)
+      toast({
+        title:"Subscription Failed",
+        description:"OOPS! some error occured. Please try again later."
+      })
+  }
+  }
   return (
     <footer
       style={{ backgroundImage: "linear-gradient(to bottom, #F3F4F6, #F7D7AC)" }}
@@ -29,7 +66,7 @@ const Footer: React.FC<FooterProps> = () => {
               <Link href="/blogs">Blogs</Link>
               <Link href="/contact">Contact</Link>
             </nav>
-            <form className="text-[#474747] flex flex-col">
+            <form onSubmit={handleSignup} className="text-[#474747] flex flex-col">
               <p className="text-lg font-semibold text-center md:text-left">Sign up for alerts</p>
               <p className="text-md text-neutral-500 text-center md:text-left">
                 New adventures are added weekly
@@ -38,13 +75,15 @@ const Footer: React.FC<FooterProps> = () => {
                 <input
                   type="email"
                   placeholder="Email"
+                  value={email}
+                  onChange={(e)=>setEmail(e.target.value)}
                   className="py-2 outline-none px-4 rounded-md"
                 />
                 <button
                   type="submit"
                   className="bg-[#FF9501] w-fit transition-all hover:bg-[#ffab35] py-2 px-4 text-white font-semibold rounded-md"
                 >
-                  Search
+                  Signup
                 </button>
               </div>
             </form>
